980a1a037b505d6097fefbc9b3e14bd020a6715a
Mehmox <71236327+Mehmox@users.noreply.github.com>
2024-11-06 21:14:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+    transports: ['websocket', 'polling']
+});
 
-// İstemciye socket.io.js dosyasını sağlayabilmek için statik dosya servisi ayarla
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
     console.log('Bir kullanıcı bağlandı.');
-    for (let inner in socket) {
-        console.log(inner)
-    }
     socket.on('mesaj', (msg) => {
         console.log('Mesaj: ' + msg);
         io.emit('mesaj', msg);
